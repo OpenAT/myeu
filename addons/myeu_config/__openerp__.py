108af11f5848451d6b2105fd539ea0ffdeadb95a
Michael Karrer <michaelkarrer81@gmail.com>
2017-08-03 00:34:58 +0200

    [ADD] __openerp__.py for the default instance addon

---
+++ addons/myeu_config/__openerp__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'myeu_config',
+    'summary': '''FS-Online myeu instance configuration''',
+    'description': '''
+FS-Online Instance Configuration
+================================
+
+Customer configuration for the instance myeu
+
+- Default settings
+- View modifications
+- CSS
+- Translations
+    ''',
+    'author': 'Michael Karrer (michael.karrer@datadialog.net), DataDialog',
+    'version': '1.0',
+    'website': 'https://www.datadialog.net',
+    'installable': True,
+    'depends': [
+        'fsonline',
+    ],
+    'data': [],
+}
+
+    
